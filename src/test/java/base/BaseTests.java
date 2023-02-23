ec0d728633d7c1e0f2c67a0526c994949dd235c1
Eitan Lichtenstein <eitanlich2000@gmail.com>
2023-02-23 19:17:04 +0200

    Proper baseTest

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,4 +1,4 @@
-package basePage;
+package base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,38 +8,56 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
-public class BasePage {
+import pages.HomePage;
 
-	public static WebDriver driver;
+public class BaseTests {
+
+	private WebDriver driver;
 	private String url;
 	private Properties prop;
+	protected HomePage homePage;
 
-	public BasePage() throws IOException {
+
+	public BaseTests() throws IOException {
 		prop = new Properties();
 		FileInputStream data = new FileInputStream(
-				System.getProperty("user.dir") + "\\src\\main\\java\\resources\\config.properties");
+				System.getProperty("user.dir") + "\\resources\\config.properties");
 		prop.load(data);
 	}
 
-	public WebDriver getDriver() {
+	@BeforeClass
+	public void setUp() {
+		driver = getDriver();
+		driver.get(getUrl());
+		driver.manage().window().maximize();
+		homePage = new HomePage(driver);
+	}
 
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+	public WebDriver getDriver() {
 		String browser = prop.getProperty("browser");
 
 		switch (browser) {
 		case "chrome":
 			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\chromedriver.exe");
+					System.getProperty("user.dir") + "\\resources\\chromedriver.exe");
 			driver = new ChromeDriver();
 			break;
 		case "firefox":
 			System.setProperty("webdriver.gecko.driver",
-					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\geckodriver.exe");
+					System.getProperty("user.dir") + "\\resources\\geckodriver.exe");
 			driver = new FirefoxDriver();
 			break;
 		case "IE":
 			System.setProperty("webdriver.ie.driver	",
-					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\IEDriver.exe");
+					System.getProperty("user.dir") + "\\resources\\IEDriver.exe");
 			driver = new InternetExplorerDriver();
 			break;
 		default:
